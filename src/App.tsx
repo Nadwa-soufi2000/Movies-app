49bd5a1c8ce671bb4d12d5bc98f455bd7465571f
nadwaso2000 <nadwaso2000@gmail.com>
2025-12-31 14:16:33 +0200

    'update'

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+       <Button variant="default" className="bg-black">Click</Button>
+       <h1 className="text-red-500">Hellow</h1>
+    </>
+  )
+}
+
+export default App
